977a69f56c3c5fa6be8eb641715f7454990f38fd
SDW-SWD <wickramsuriyashehan@gmail.com>
2024-06-21 23:33:16 +0530

    jwt token expiration time and signature configure

--- Ponit Of Sales System/src/main/java/org/CypsoLabs/config/security/SecurityConstance.java
+++ Ponit Of Sales System/src/main/java/org/CypsoLabs/config/security/SecurityConstance.java
@@ -1,6 +1,6 @@
 package org.CypsoLabs.config.security;
 
 public class SecurityConstance {
-    public static  final long JWT_EXPIRATION=70000;
-    public static final String JWT_SECRET="Secret";
+    public static  final long JWT_EXPIRATION=90000;
+    public static final String JWT_SECRET="AH9R6CEy53oEJIfYCHv0H7iPW/WH/NoSF9K89f5ah2I=";
 }

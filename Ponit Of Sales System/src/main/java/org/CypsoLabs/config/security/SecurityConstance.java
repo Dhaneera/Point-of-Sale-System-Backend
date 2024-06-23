aa4fef1e0f9efb46d4582a29f99ee9631f62e7eb
SDW-SWD <wickramsuriyashehan@gmail.com>
2024-06-23 13:37:13 +0530

    access token expiration time setting up in  as a security constance

--- Ponit Of Sales System/src/main/java/org/CypsoLabs/config/security/SecurityConstance.java
+++ Ponit Of Sales System/src/main/java/org/CypsoLabs/config/security/SecurityConstance.java
@@ -1,7 +1,7 @@
 package org.CypsoLabs.config.security;
 
 public class SecurityConstance {
-    public static  final long JWT_EXPIRATION=90000;//15 minutes
+    public static  final long JWT_EXPIRATION=10000;//15 minutes
     public static final String JWT_SECRET="AH9R6CEy53oEJIfYCHv0H7iPW/WH/NoSF9K89f5ah2I=";
     public static final long JWT_REFRESH_EXPIRATION =604800000; //7 days
 }
